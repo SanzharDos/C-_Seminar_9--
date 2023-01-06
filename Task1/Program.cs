47055c033020b2cab20ea512ea8a5da0c1ccfd71
Sanzhar <dos.0227@yandex.ru>
2023-01-06 19:18:29 +0600

    Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Посчитать сумму всех целых чисел от 1 до n, где n вводится с клавиатуры
+
+Console.WriteLine("Введите целое число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма чисел от 1 до {N} равна {Summa(N)}");
+Console.WriteLine($"Сумма чисел от 1 до {N} через рекурсию равна {SummaRec(N)}");
+
+int Summa(int N)
+{
+    int res = 0;
+    while (true)
+    {
+        res += N;
+        N--;
+        if (N == 0)
+            break;
+    }
+    return res;
+}
+
+int SummaRec(int N)
+{
+    if (N == 0) return 0; // алгоритм прекращает свою работу, когда N приравнивается 0, иначе:
+    return N + SummaRec(N - 1);
+}
